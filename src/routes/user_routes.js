56d4ccc30dc775df9d81659ebb8ce6ff62c4d90d
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-02-03 20:56:27 +0800

    backend_v.2.3

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import * as UserController  from '../controllers/user_controller.js'; // Adjust the path as necessary
-import { authenticateToken } from '../middleware/auth.js'; // Adjust the path as necessary
 
 const userRoutes = Router();
 
@@ -9,9 +8,6 @@ userRoutes.post('/create', UserController.create_user);
 userRoutes.post('/login_user', UserController.login_user);
 userRoutes.get('/get_all_user', UserController.get_all_user);
 
-userRoutes.get('/protected', authenticateToken, (req, res) => {
-    res.status(200).json({ message: 'This is a protected route', data: req.user });
-});
 
 
 export default userRoutes;
