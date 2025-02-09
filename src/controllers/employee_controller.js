173c16845fccc806d1833f4dbe43d1bbfa0094aa
megaxsolutions <megaxsolutions@gmail.com>
2025-02-09 20:21:32 +0800

    updates

--- src/controllers/employee_controller.js
+++ src/controllers/employee_controller.js
@@ -142,7 +142,7 @@ export const login_employee = asyncHandler(async (req, res) => {
         const [login] = await db.promise().query(sql, [emp_ID]);
         const dateObject = new Date(login[0]['expiry_date']);
         const expiryDate = dateObject.toISOString().split('T')[0];
-
+        console.log(login);
         if(storeCurrentDate(0, 'months') > expiryDate) {
             return res.status(400).json({ error: 'Your account has expired. Please contact the administrator for assistance.' });        
         }
