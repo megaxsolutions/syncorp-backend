1f0d35bb5900e09a2a9e40af3fb4cda606b9dcc4
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-02-13 15:51:54 +0800

    Update recovery_controller.js

--- src/controllers/recovery_controller.js
+++ src/controllers/recovery_controller.js
@@ -86,7 +86,7 @@ export const otp_verification = asyncHandler(async (req, res) => {
         const date1 = new Date(convertToUTC(storeCurrentDateTime(0, 'months')));
         const date2 = new Date(convertToUTC(otp[0]['date_time']));
 
-        const time1 = formatTime(date1);
+        const time1 = formatTime(otp[0]['date_time']);
         const time2 = formatTime(date2);
         
         return res.status(200).json({ data1: otp[0]['date_time'], date1: date1, date2: date2, time1: time1, time2: time2, test:123 });
