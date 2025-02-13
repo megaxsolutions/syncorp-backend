d0deb7a569245fa39121eb26cc78c7a3b03441c2
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-02-13 15:47:01 +0800

    Update recovery_controller.js

--- src/controllers/recovery_controller.js
+++ src/controllers/recovery_controller.js
@@ -87,11 +87,12 @@ export const otp_verification = asyncHandler(async (req, res) => {
         const date2 = new Date(otp[0]['date_time']);
         const time1 = formatTime(date1);
         const time2 = formatTime(date2);
+        
         return res.status(200).json({ data1: otp[0]['date_time'], date1: date1, date2: date2, time1: time1, time2: time2 });
 
-        if(time1 > time2) {
-            return res.status(400).json({ error: 'Your OTP has expired. Please request a new one.' });
-        }
+        // if(time1 > time2) {
+        //     return res.status(400).json({ error: 'Your OTP has expired. Please request a new one.' });
+        // }
 
         if(otp_code != otp[0]['code']) {
             return res.status(400).json({ error: 'Invalid OTP. Please try again.' });
