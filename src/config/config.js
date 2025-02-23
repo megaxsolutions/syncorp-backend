07056063ecbfc9c9203caa59858580e3e22db125
megaxsolutions <megaxsolutions@gmail.com>
2025-02-23 21:36:43 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ dotenv.config();
 let db;
 
 function handleDisconnect() {
-  db = mysql.createPool({
+  db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
