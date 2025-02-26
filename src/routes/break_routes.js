0f2880dc1f0c780b5eb402ac00a4c79f43806489
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-02-26 12:09:32 +0800

    Update break_routes.js

--- src/routes/break_routes.js
+++ src/routes/break_routes.js
@@ -10,7 +10,7 @@ const breakRoutes = Router();
 breakRoutes.post('/add_break', BreakController.create_break);
 breakRoutes.put('/update_break/:emp_id', BreakController.update_break_break_out);
 breakRoutes.get('/get_all_break', BreakController.get_all_break);
-breakRoutes.get('/get_all_user_break', BreakController.get_all_user_break);
+breakRoutes.get('/get_all_user_break/:emp_id', BreakController.get_all_user_break);
 
 
 export default breakRoutes;
