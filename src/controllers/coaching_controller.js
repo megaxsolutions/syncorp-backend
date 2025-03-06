48f78adb457e3cee6b316543bf58433aad4c668f
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-03-06 19:04:47 +0800

    Update coaching_controller.js

--- src/controllers/coaching_controller.js
+++ src/controllers/coaching_controller.js
@@ -31,7 +31,8 @@ export const create_coaching = asyncHandler(async (req, res) => {
         // Return the merged results in the response
         return res.status(200).json({ success: 'Coaching successfully created.' });
     } catch (error) {
-        return res.status(500).json({ error: 'Failed to create coaching.', data: error });
+        return res.status(500).json({ data: error });
+        //return res.status(500).json({ error: 'Failed to create coaching.', data: error });
     }
 });
 
