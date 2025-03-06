ac516b045cfd0b367d7efb32fc7fc2ace837c75d
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-03-06 16:52:11 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [process.env.CLIENT_BASE_URL, 'http://localhost:5173'], // Adjust this to your React app's URL
+        origin: [process.env.CLIENT_BASE_URL, 'http://localhost:5173', 'https://test-bulletin-one.vercel.app'], // Adjust this to your React app's URL
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
