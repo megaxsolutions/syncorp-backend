222408993c8b046c8cab3dbbdd4b1db597bef2d1
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-04-01 15:17:25 +0800

    Update submission_controller.js

--- src/controllers/lms/submission_controller.js
+++ src/controllers/lms/submission_controller.js
@@ -25,7 +25,7 @@ export const create_submission = asyncHandler(async (req, res) => {
 
     try {
         const sql = 'INSERT INTO submissions (courseID, categoryID, emp_ID, questionID, answer, correct_answer, datetime_submitted) VALUES (?, ?, ?, ?, ?, ?, ?)';
-        const [insert_data_submission] = await db.query(sql, [course_id, category_id, emp_id, question_id, answer, correct_answer, storeCurrentDateTime(0, 'hours')]);
+        const [insert_data_submission] = await db2.query(sql, [course_id, category_id, emp_id, question_id, answer, correct_answer, storeCurrentDateTime(0, 'hours')]);
       
         // Return the merged results in the response
         return res.status(200).json({ success: 'Submission successfully created.' });
@@ -42,7 +42,7 @@ export const update_submission = asyncHandler(async (req, res) => {
     try {
         const sql = `UPDATE submissions SET courseID = ?, categoryID = ?, questionID = ?, answer = ?, correct_answer = ? WHERE id = ?`;
 
-        const [update_data_submission] = await db.query(sql, [course_id, category_id, question_id, answer, correct_answer, submission_id]);
+        const [update_data_submission] = await db2.query(sql, [course_id, category_id, question_id, answer, correct_answer, submission_id]);
 
         // Return the merged results in the response
         return res.status(200).json({ success: 'Submission successfully updated.' });
@@ -60,7 +60,7 @@ export const get_all_submission = asyncHandler(async (req, res) => {
         DATE_FORMAT(datetime_submitted, '%Y-%m-%d %H:%i:%s') AS datetime_submitted
         FROM submissions`; // Use a parameterized query
                                   
-        const [submission] = await db.query(sql);
+        const [submission] = await db2.query(sql);
 
         // Return the merged results in the response
         return res.status(200).json({ data: submission });
@@ -76,7 +76,7 @@ export const delete_submission = asyncHandler(async (req, res) => {
     try {
         const sql = 'DELETE FROM submissions WHERE id = ?';
 
-        const [result] = await db.query(sql, [submission_id]);
+        const [result] = await db2.query(sql, [submission_id]);
 
         if (result.affectedRows === 0) {
             return res.status(404).json({ error: 'Submission not found.' });
