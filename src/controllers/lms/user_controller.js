30be5636e6802c3cc7eb0c1026655274703abf88
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-04-01 20:48:03 +0800

    Update user_controller.js

--- src/controllers/lms/user_controller.js
+++ src/controllers/lms/user_controller.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
-import { db2 } from '../../config/config.js'; // Import the database connection
+import { db2, db } from '../../config/config.js'; // Import the database connection
 
 import moment from 'moment-timezone';
 
@@ -36,15 +36,65 @@ function storeCurrentDate(expirationAmount, expirationUnit) {
 }
 
 export const create_user = asyncHandler(async (req, res) => {
-    const { emp_id } = req.body;
+    const { array_employee_emp_id  } = req.body;
+    const insertValues = [];
+    let employees_affected = 0;
+    const existingSchedulesMap = new Map();
 
     try {
-        const sql = 'INSERT INTO users (emp_ID, date_created) VALUES (?, ?)';
+        const sql       = 'SELECT * FROM login'; // Use a parameterized query
+        const sqlInsert = 'INSERT INTO users (emp_ID, date_created) VALUES ?';
+        const sqlSelect = `SELECT id, emp_ID, 
+        DATE_FORMAT(date_created, '%Y-%m-%d') AS date_created
+        FROM users WHERE emp_ID = ?;`;
 
-        const [insert_data_submission] = await db2.query(sql, [emp_id, storeCurrentDate(0, 'hours')]);
-      
-        // Return the merged results in the response
-        return res.status(200).json({ success: 'User successfully created.' });
+
+        const lms_users = await Promise.all(
+            array_employee_emp_id.map(async (emp_id) => {
+                const [result] = await db2.query(sqlSelect, [emp_id]);
+                return result;
+            })
+        );
+
+
+        lms_users.flat().forEach(user => {
+            const key = `${user.emp_ID}`;
+            existingSchedulesMap.set(key, true);
+        });
+
+         // Loop through each employee and day to prepare the insertion data
+         for (const emp_id of array_employee_emp_id) {
+            let count_employees = 0;
+            const key = `${emp_id}`;
+
+            // Check if the (emp_id, day) combination is already in the map (i.e., the shift exists)
+            if (!existingSchedulesMap.has(key)) {
+                insertValues.push([emp_id, storeCurrentDate(0, 'hours')]);
+                ++count_employees;
+            }
+
+            // If at least one schedule was added for this employee, increment the affected count
+            if (count_employees > 0) {
+                employees_affected++;
+            }
+        }
+
+
+        // If there are any values to insert, perform a batch insert
+        if (insertValues.length > 0) {
+            await db2.query(sqlInsert, [insertValues]);
+        }
+
+        const [users] = await db.query(sql);
+
+
+        if (users.length === array_employee_emp_id.length) {
+            return res.status(200).json({ success: 'All employees have been added to the LMS.' });
+        }
+
+        return res.status(200).json({ 
+            success: `${employees_affected} employee${employees_affected >= 2 ? 's' : ''} have been added to the LMS.` 
+        });        // Return the merged results in the response
     } catch (error) {
         return res.status(500).json({ error: 'Failed to create user.' });
     }
