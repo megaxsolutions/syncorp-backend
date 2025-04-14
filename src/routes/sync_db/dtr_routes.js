697265d75d9fd4a5dce0c108a193e2561fc87b9a
Gerald C. Manatad <gerald.manatad@evsu.edu.ph>
2025-04-14 14:17:55 +0800

    updated

--- src/routes/sync_db/dtr_routes.js
+++ src/routes/sync_db/dtr_routes.js
@@ -7,7 +7,6 @@ const dtrRoutes = Router();
 
 dtrRoutes.get('/get_all_dtr', DTRController.get_all_dtr);
 dtrRoutes.get('/get_all_user_dtr/:emp_id/:cutoff_id', DTRController.get_all_user_dtr);
-dtrRoutes.get('/get_all_user_dtr_computation/:emp_id/:cutoff_id', DTRController.get_all_user_dtr_computation);
 dtrRoutes.get('/get_all_user_dtr_admin/:cutoff_id', DTRController.get_all_user_dtr_admin);
 
 
