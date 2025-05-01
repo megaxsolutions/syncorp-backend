d1c93c9cec2c2b40496031d04f5ae29a945c6b98
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-05-01 17:28:09 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -107,12 +107,12 @@ const port = process.env.PORT;
 // const wss = new WebSocketServer({ port: 8001 });
 
 
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.text()); 
-app.use(express.json({ limit: '2000mb' }));
-app.use(express.urlencoded({ extended: true, limit: '2000mb' }));
+//app.use(express.json({ limit: '2000mb' }));
+//app.use(express.urlencoded({ extended: true, limit: '2000mb' }));
 
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
